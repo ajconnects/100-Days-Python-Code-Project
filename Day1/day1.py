06b87da3f6bc5f25e8c90b9246e8d2e339afb56f
Aj connect <ajiriiviero2021@gmail.com>
2024-03-15 22:22:35 +0100

    add

---
+++ Day1/day1.py
@@ -0,0 +1,5 @@
+#Day1 Task Variable and Input
+print("Welcome to the band name generator.")
+city_name = input("What's the name of the city you grew up in?\n")
+pet_name = input("What's your pet's name?\n")
+print("Your band name could be", city_name.title(), pet_name.title())
